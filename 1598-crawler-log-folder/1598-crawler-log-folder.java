482f860e97f6b6de43d6723eaa9a0a9ac53b037e
Edicson <163763856+lavapatos@users.noreply.github.com>
2025-01-26 16:38:28 -0300

    Time: 2 ms (32.85%), Space: 42.5 MB (18.82%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        var stack = new Stack<String>();
+        for(var log : logs) {
+            if(log.equals("../")) {
+                if(!stack.empty()) {stack.pop();}
+            }
+            else if(log.equals("./")){continue;}
+            else {stack.push(log);}
+        }
+        return stack.size();
+    }
+}

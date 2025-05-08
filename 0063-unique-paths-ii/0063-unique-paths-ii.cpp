6646c50236da52a94e42cd482df6637c44ab9894
Edicson <163763856+lavapatos@users.noreply.github.com>
2025-05-08 01:00:16 -0400

    Time: 0 ms (100%), Space: 11.4 MB (88.6%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if (obstacleGrid[0][0]) {return 0;}
+        if (obstacleGrid.empty() || obstacleGrid[0][0]) {return 0;}
+
         int rows = obstacleGrid.size();
         int cols = obstacleGrid[0].size();
+        vector<int> dp(cols, 0);
+        dp[0] = 1;
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                obstacleGrid[i][j] = !obstacleGrid[i][j];
+                if (obstacleGrid[i][j] == 1) {dp[j] = 0;}
+                else if (j > 0) {dp[j] += dp[j-1];}
             }
         }
-
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
-                if (!obstacleGrid[i][j]) {continue;}
-                else {
-                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
-                }
-            }
-        }
-        return obstacleGrid[rows-1][cols-1];
-
+        return dp[cols-1];
     }
 };

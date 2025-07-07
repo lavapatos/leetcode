7ebf156083301b90a3b53b0b8d6b622654442f92
Edicson <163763856+lavapatos@users.noreply.github.com>
2025-07-07 17:28:32 -0400

    Time: 3 ms (24.77%), Space: 17.9 MB (7.34%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -1,12 +1,7 @@
-class Solution(object):
-    def scoreOfString(self, s):
+class Solution:
+    def scoreOfString(self, s: str) -> int:
         ans = 0
-        """
-        :type s: str
-        :rtype: int
-        """
         for i in range(len(s)-1):
             ans += abs(ord(s[i])-ord(s[i+1]))
         
         return ans
-        

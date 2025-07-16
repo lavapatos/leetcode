b24166df8935cdf7d4d823d97e72a99d64b76acd
Edicson <163763856+lavapatos@users.noreply.github.com>
2025-07-16 15:34:06 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, word):
+        vowels = 'aAeEiIoOuU'
+        nCount = 0
+        vCount = 0
+        if len(word) < 3:
+            return False
+        for c in word:
+            if ((ord(c)<48) or (ord(c)>57 and ord(c)<65) or (ord(c)>90 and ord(c)<97) or (ord(c)>122)):
+                return False
+            if ord(c)>47 and ord(c)<58:
+                nCount += 1
+            if c in vowels:
+                vCount += 1
+        return vCount and len(word) > vCount + nCount

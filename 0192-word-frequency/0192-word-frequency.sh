ff65aded985498d0973a88c744f49b2a20da421e
Edicson <163763856+lavapatos@users.noreply.github.com>
2025-10-01 15:33:56 -0300

    Time: 76 ms (54.95%), Space: 3.9 MB (85.3%) - LeetHub

--- 0192-word-frequency/0192-word-frequency.sh
+++ 0192-word-frequency/0192-word-frequency.sh
@@ -1,2 +1,2 @@
 # Read from the file words.txt and output the word frequency list to stdout.
-cat words.txt | sort | uniq -c | sort -r | awk '{print $2, $1}'
+cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -r | awk '{print $2, $1}'
